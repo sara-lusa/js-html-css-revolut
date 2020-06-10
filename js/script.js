4c65eb8c1049a5f8803bde754e94699afee14a86
Sara <saracorsob@gmail.com>
2020-06-10 16:55:22 +0200

    Scrittura js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+$(document).ready(
+  function() {
 
+    var eventoMenu = $('.menu .with-dropdown');
+
+    eventoMenu.mouseenter(
+      function() {
+        $('.dropdown').addClass('active');
+      }
+    );
+
+    eventoMenu.mouseleave(
+      function() {
+        $('.dropdown').removeClass('active');
+      }
+    );
+
+    // eventoMenu.click(
+    //   function() {
+    //     $('.dropdown').removeClass('active');
+    //   }
+    // );
+
+  }
+);
